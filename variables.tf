fdfd384b1c2756a7c40d49bd534203f5446398c9
aknysh <andriy.knysh@gmail.com>
2018-03-18 12:50:09 -0400

    Init files

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "group_name" {
+  type        = "string"
+  description = "The name of the Group"
+}
+
+variable "user_names" {
+  type        = "list"
+  description = "A list of IAM User names to associate with the Group"
+}
+
+variable "member_account_id" {
+  type        = "string"
+  description = "The ID of the member account to grant access permissions to the users in the Group"
+}
+
+variable "role_name" {
+  type        = "string"
+  default     = "OrganizationAccountAccessRole"
+  description = "The name of the role to grant permissions to delegated IAM users in the master account"
+}
+
+variable "policy_name" {
+  type        = "string"
+  default     = "OrganizationAccountAccessPolicy "
+  description = "The name of the policy to attach to the Group"
+}
