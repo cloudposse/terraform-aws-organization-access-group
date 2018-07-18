5b2e25051215f95bcfa6a1c75e21c0804cd4e0d3
vadim-hleif <vadzim.hleif@gmail.com>
2018-07-18 14:41:18 +0300

    Migrate readme yaml

--- output.tf
+++ output.tf
@@ -1,23 +1,29 @@
 output "group_name" {
-  value = "${aws_iam_group.default.name}"
+  value       = "${aws_iam_group.default.name}"
+  description = "The Group's name"
 }
 
 output "group_id" {
-  value = "${aws_iam_group.default.id}"
+  value       = "${aws_iam_group.default.id}"
+  description = "The Group's ID"
 }
 
 output "group_unique_id" {
-  value = "${aws_iam_group.default.unique_id}"
+  value       = "${aws_iam_group.default.unique_id}"
+  description = "Group's unique ID assigned by AWS"
 }
 
 output "group_arn" {
-  value = "${aws_iam_group.default.arn}"
+  value       = "${aws_iam_group.default.arn}"
+  description = "The ARN assigned by AWS for the Group"
 }
 
 output "policy_name" {
-  value = "${join("", coalescelist(aws_iam_group_policy.without_mfa.*.name, aws_iam_group_policy.with_mfa.*.name))}"
+  value       = "${join("", coalescelist(aws_iam_group_policy.without_mfa.*.name, aws_iam_group_policy.with_mfa.*.name))}"
+  description = "The name of the policy"
 }
 
 output "policy_id" {
-  value = "${join("", coalescelist(aws_iam_group_policy.without_mfa.*.id, aws_iam_group_policy.with_mfa.*.id))}"
+  value       = "${join("", coalescelist(aws_iam_group_policy.without_mfa.*.id, aws_iam_group_policy.with_mfa.*.id))}"
+  description = "The policy ID"
 }
