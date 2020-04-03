188b990327d6febbf67a64ed621f3c7659addd85
Nick Pellant <nick@nickpellant.com>
2020-04-03 18:29:16 +0100

    Perform terraform 0.12 upgrade

--- main.tf
+++ main.tf
@@ -1,27 +1,33 @@
 module "label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
-  namespace  = "${var.namespace}"
-  name       = "${var.name}"
-  stage      = "${var.stage}"
-  delimiter  = "${var.delimiter}"
-  attributes = "${var.attributes}"
-  tags       = "${var.tags}"
+  namespace  = var.namespace
+  name       = var.name
+  stage      = var.stage
+  delimiter  = var.delimiter
+  attributes = var.attributes
+  tags       = var.tags
 }
 
 locals {
-  enabled      = "${var.enabled == "true" ? true : false }"
-  require_mfa  = "${var.require_mfa == "true" ? true : false}"
-  role_arns    = ["${values(var.role_arns)}"]
-  role_aliases = ["${keys(var.role_arns)}"]
+  enabled      = var.enabled == "true" ? true : false
+  require_mfa  = var.require_mfa == "true" ? true : false
+  role_arns    = [values(var.role_arns)]
+  role_aliases = [keys(var.role_arns)]
 }
 
 resource "null_resource" "role" {
-  count = "${length(values(var.role_arns))}"
+  count = length(values(var.role_arns))
 
   triggers = {
-    account_id = "${element(split(":", element(local.role_arns, count.index)), 4)}"
-    role_name  = "${element(split("/", element(split(":", element(local.role_arns, count.index)), 5)), 1)}"
-    alias      = "${element(local.role_aliases, count.index)}"
+    account_id = element(split(":", element(local.role_arns, count.index)), 4)
+    role_name = element(
+      split(
+        "/",
+        element(split(":", element(local.role_arns, count.index)), 5),
+      ),
+      1,
+    )
+    alias = element(local.role_aliases, count.index)
   }
 
   lifecycle {
@@ -31,27 +37,35 @@ resource "null_resource" "role" {
 
 # https://www.terraform.io/docs/providers/aws/r/iam_group.html
 resource "aws_iam_group" "default" {
-  count = "${local.enabled ? 1 : 0}"
-  name  = "${module.label.id}"
+  count = local.enabled ? 1 : 0
+  name  = module.label.id
 }
 
 # https://www.terraform.io/docs/providers/aws/r/iam_user_group_membership.html
 resource "aws_iam_user_group_membership" "default" {
-  count  = "${local.enabled && length(var.user_names) > 0 ? length(var.user_names) : 0}"
-  user   = "${element(var.user_names, count.index)}"
-  groups = ["${join("", aws_iam_group.default.*.id)}"]
+  count  = local.enabled && length(var.user_names) > 0 ? length(var.user_names) : 0
+  user   = element(var.user_names, count.index)
+  groups = [join("", aws_iam_group.default.*.id)]
 }
 
 # https://www.terraform.io/docs/providers/aws/d/iam_policy_document.html
 data "aws_iam_policy_document" "with_mfa" {
-  count = "${local.enabled && local.require_mfa ? 1 : 0}"
+  count = local.enabled && local.require_mfa ? 1 : 0
 
   statement {
     actions = [
       "sts:AssumeRole",
     ]
 
-    resources = ["${local.role_arns}"]
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibility in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    resources = [local.role_arns]
 
     condition {
       test     = "Bool"
@@ -64,29 +78,38 @@ data "aws_iam_policy_document" "with_mfa" {
 }
 
 resource "aws_iam_group_policy" "with_mfa" {
-  count  = "${local.enabled && local.require_mfa ? 1 : 0}"
-  name   = "${module.label.id}"
-  group  = "${join("", aws_iam_group.default.*.id)}"
-  policy = "${data.aws_iam_policy_document.with_mfa.json}"
+  count  = local.enabled && local.require_mfa ? 1 : 0
+  name   = module.label.id
+  group  = join("", aws_iam_group.default.*.id)
+  policy = data.aws_iam_policy_document.with_mfa[0].json
 }
 
 data "aws_iam_policy_document" "without_mfa" {
-  count = "${local.enabled && local.require_mfa == false ? 1 : 0}"
+  count = local.enabled && local.require_mfa == false ? 1 : 0
 
   statement {
     actions = [
       "sts:AssumeRole",
     ]
 
-    resources = ["${local.role_arns}"]
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibility in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    resources = [local.role_arns]
 
     effect = "Allow"
   }
 }
 
 resource "aws_iam_group_policy" "without_mfa" {
-  count  = "${local.enabled && local.require_mfa == false ? 1 : 0}"
-  name   = "${module.label.id}"
-  group  = "${join("", aws_iam_group.default.*.id)}"
-  policy = "${data.aws_iam_policy_document.without_mfa.json}"
+  count  = local.enabled && local.require_mfa == false ? 1 : 0
+  name   = module.label.id
+  group  = join("", aws_iam_group.default.*.id)
+  policy = data.aws_iam_policy_document.without_mfa[0].json
 }
+
